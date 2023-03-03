d3212b6877e2220936f0c1466f58dec16d741a09
EmanuelMarcolongo <emanuelmarcolongo@gmail.com>
2023-03-03 13:15:13 -0300

    feat: seed with ticketTypes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -16,6 +16,33 @@ async function main() {
     });
   }
 
+  await prisma.ticketType.create({
+    data: {
+      name: "remote",
+      price: 100,
+      isRemote: true,
+      includesHotel: false,
+    }
+  })
+
+  await prisma.ticketType.create({
+    data: {
+      name: "presencialSemHotel",
+      price: 250,
+      isRemote: false,
+      includesHotel: false,
+    }
+  })
+
+  await prisma.ticketType.create({
+    data: {
+      name: "presencialComHotel",
+      price: 600,
+      isRemote: false,
+      includesHotel: true,
+    }
+  })
+
   console.log({ event });
 }
 
